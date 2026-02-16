2f6208b72aab877e685d00d97b40161f994ab5e4
llucs <llucsegala29@gmail.com>
2026-02-16 21:38:54 +0100

    Header de la web

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react';
 import './Fondo_navbar_homeBut.css';
-
+import './header.css';
 function App() {
   // 1. Aquí anirà la "memòria" (estat) més endavant
   // Simulem que l'usuari NO està loguejat (false)
   // Si canvies aquest false per true manualment, veuràs el canvi a la web
-  const [isLoggedIn] = useState(true);
+  const [isLoggedIn] = useState(false);
  return (
     <div className="mobil-wrapper">
+      <header className="vacy-header">
+        <div className="vacy-logo-container">
+          <span className="vacy-letter">V</span>
+          <span className="vacy-letter">A</span>
+          <div className="vacy-letter-c-wrapper">
+            <span className="vacy-letter">C</span>
+          </div>
+          <span className="vacy-letter">Y</span>
+        </div>
+        <p className="vacy-slogan">Ayuda a tu nevera a vaciarse con Vacy</p>
+      </header>
       <nav className="navbar-superior">
         {/* MIG: El Buscador */}
         <div className="nav-center">
